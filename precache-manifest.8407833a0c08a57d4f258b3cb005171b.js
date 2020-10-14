50cabc93d37d555a71faec1f3862af38425da689
Ayush Pandey <ayushpandey8439@gmail.com>
2020-10-14 03:47:21 +0200

    Updates

---
+++ precache-manifest.8407833a0c08a57d4f258b3cb005171b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7aef4b807e0ff9120fb6666e5c3bd2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0860e4f7aa4b0b27cfe2",
+    "url": "/portfolio/static/css/main.f99b5bee.chunk.css"
+  },
+  {
+    "revision": "72aa333209ce55f53c9c",
+    "url": "/portfolio/static/js/2.29ec2c88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.29ec2c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0860e4f7aa4b0b27cfe2",
+    "url": "/portfolio/static/js/main.8b73526f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ed4a2ac7c5425cb867b384068ada3c0b",
+    "url": "/portfolio/static/media/background.ed4a2ac7.mp4"
+  }
+]);
