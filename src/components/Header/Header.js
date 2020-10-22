0270f0e50e3a7a21218d772b120b3734f35789e8
Ayush Pandey <ayushpandey8439@gmail.com>
2020-10-22 19:56:57 +0200

    Show menu button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 function Header(props) {
+  const [state, setState] = useState({ displayMenu: false });
+  function showMenu() {
+    setState({ displayMenu: !state.displayMenu });
+  }
   return (
     <div>
       <div className="header">
-        <MenuIcon></MenuIcon>
+        {!state.displayMenu && (
+          <MenuIcon onClick={showMenu} className="hamburger__menu"></MenuIcon>
+        )}
+        {state.displayMenu && (
+          <CloseIcon onClick={showMenu} className="hamburger__menu"></CloseIcon>
+        )}
       </div>
     </div>
   );
